c4231c1f3ffc529f40b83c899898dc0d15665cee
Christopher Baker <me@christopherbaker.net>
2012-09-27 13:17:49 -0500

    Fixes.

--- 2012/Spring/Activated Objects/Week 5/Arduino/MultiButton_SerialReceiver/MultiButton_SerialReceiver.ino
+++ 2012/Spring/Activated Objects/Week 5/Arduino/MultiButton_SerialReceiver/MultiButton_SerialReceiver.ino
@@ -1,15 +1,16 @@
 int ledPinArray[] = {
   2, 3, 4}; // the pin addresses for our buttons
 
-int count = 0;
+int buttonIndex = 0;
+const int nButtons = 3;
 
 byte ledStateBits; // we are using this as a binary number
 
 void setup() {
   Serial.begin(9600); // start the serial mode so we can debug on the computer
 
-  for(count=0; count<3;count++) {
-    pinMode(ledPinArray[count], OUTPUT);
+  for(buttonIndex=0; buttonIndex< nButtons;buttonIndex++) {
+    pinMode(ledPinArray[buttonIndex], OUTPUT);
   } 
 }
 
@@ -18,8 +19,8 @@ void loop() {
     ledStateBits = Serial.read(); // read the incoming ButtonStateBits
   }
 
-  for(count=0; count<3;count++) {
-    digitalWrite(ledPinArray[count],bitRead(ledStateBits, count));
+  for(buttonIndex=0; buttonIndex < nButtons; buttonIndex++) {
+    digitalWrite(ledPinArray[buttonIndex],bitRead(ledStateBits, buttonIndex));
   }
 
   delay(10);
